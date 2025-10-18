a063c60577f6978c83dab35b0e98eb0f17a9e9d3
kameshbimsara <kameshbimsara2245@gmail.com>
2025-10-18 17:14:06 +0530

    create loginPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import RegisterPage from './pages/RegisterPage/RegisterPage'
 import LogingPage from './pages/LogingPage/LogingPage'
+import HomePage from './pages/HomePage/HomePage';
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<LogingPage />} />
         <Route path="/register" element={<RegisterPage />} />
+
+        <Route path="/home" element={
+          localStorage.getItem("afsd-9") ? <HomePage /> : <Navigate to="/login" />
+        } />
+
+        <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </BrowserRouter>
   )
